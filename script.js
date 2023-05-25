fe2bd03606ad60116a6b9b2903406683eb107c56
AngelNaEs <angelespino99@gmail.com>
2023-05-25 08:38:05 -0600

    Encriptador Terminado

--- script.js
+++ script.js
@@ -1,26 +1,99 @@
-function caesarShift(str, amount) {
-  let output = "";
-  for (let i = 0; i < str.length; i++) {
-    let ascii = str[i].charCodeAt(0);
-    if (ascii >= 65 && ascii <= 90) {
-      output += String.fromCharCode(((ascii - 65 + amount + 26) % 26) + 65);
-    } else if (ascii >= 97 && ascii <= 122) {
-      output += String.fromCharCode(((ascii - 97 + amount + 26) % 26) + 97);
+function encriptarCadena(cadena) {
+  var reglas = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat",
+  };
+
+  var resultado = "";
+
+  for (var i = 0; i < cadena.length; i++) {
+    var letra = cadena[i];
+    if (letra in reglas) {
+      resultado += reglas[letra];
     } else {
-      output += str[i];
+      resultado += letra;
     }
   }
-  return output;
+
+  return resultado;
 }
 
+function desencriptarCadena(cadena) {
+  var reglasInversas = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
+
+  var resultado = cadena;
+
+  for (var regla in reglasInversas) {
+    var posicion = resultado.indexOf(regla);
+    while (posicion != -1) {
+      resultado =
+        resultado.substring(0, posicion) +
+        reglasInversas[regla] +
+        resultado.substring(posicion + regla.length);
+      posicion = resultado.indexOf(regla);
+    }
+  }
+
+  return resultado;
+}
+
+// Encriptar
 document.getElementById("encryptButton").addEventListener("click", function () {
+  var noText = document.getElementById("noText");
+  noText.classList.add("ocultarElement");
+
+  var outputContainer = document.getElementById("outputContainer");
+  outputContainer.classList.remove("ocultarElement");
+
   let inputText = document.getElementById("inputText").value;
-  let encryptedText = caesarShift(inputText, 3);
+
+  let encryptedText = encriptarCadena(inputText);
+  document.getElementById("outputText").innerText = encryptedText;
+});
+// Desencriptar
+document.getElementById("decryptButton").addEventListener("click", function () {
+  var noText = document.getElementById("noText");
+  noText.classList.add("ocultarElement");
+
+  var outputContainer = document.getElementById("outputContainer");
+  outputContainer.classList.remove("ocultarElement");
+
+  let inputText = document.getElementById("inputText").value;
+
+  let encryptedText = desencriptarCadena(inputText);
   document.getElementById("outputText").innerText = encryptedText;
 });
 
-document.getElementById("decryptButton").addEventListener("click", function () {
-  let inputText = document.getElementById("inputText").value;
-  let decryptedText = caesarShift(inputText, -3);
-  document.getElementById("outputText").innerText = decryptedText;
+// Copiar
+document.getElementById("btnCopy").addEventListener("click", function () {
+  var textoACopiar = document.getElementById("outputText").innerText;
+
+  navigator.clipboard
+    .writeText(textoACopiar)
+    .then(function () {
+      showNotification("Texto copiado al portapapeles exitosamente!");
+    })
+    .catch(function (err) {
+      showNotification("No se pudo copiar el texto: " + err);
+    });
 });
+
+//Mostrar un mensaje de copiado
+function showNotification(message) {
+  var notification = document.getElementById("notification");
+  notification.textContent = message;
+  notification.classList.add("show");
+
+  setTimeout(function () {
+    notification.classList.remove("show");
+  }, 2500);
+}
